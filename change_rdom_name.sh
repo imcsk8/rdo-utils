d9ff591030f219bcdca84f9955eee5628555d655
Ivan Chavero <ichavero@redhat.com>
2016-03-17 19:53:18 -0600

    Fixes regexp
    Changed regexp for a more broad set of appearences of RDO Manager
    references.

--- change_rdom_name.sh
+++ change_rdom_name.sh
@@ -5,7 +5,7 @@
 
 for f in `find . -type f`; do
     echo "Changing: $f"
-    sed -e 's/RDO.Manager/TripleO/g' $f
-    sed -e 's/rdo.manager/tripleo/g' $f
+    sed -i -e 's/rdo.manager/tripleo/g' $f
+    sed -i -e 's/RDO..anager/TripleO/g' $f
 done
 
